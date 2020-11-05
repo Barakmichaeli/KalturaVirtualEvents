475be1a5e70342bfd0fc6d267d62dd9fe0e7177d
BarakMichaeli <barak.m@monolith.co.il>
2020-11-05 02:26:44 +0200

    change ks session to user session, in order to restrict app while there is no other security guards

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,6 @@
 
 return [
     'KALTURA_SECRET' => 'd99cb7c27013089f683b4fc8ce53f54c',
-    'KALTURA_USER_ID' => 'ce2d448d754bc485aaf96ce0d935487c',
+    'KALTURA_USER_ID' => 'barak6546@gmail.com',
     'KALTURA_PARTNER_ID' => '3085303',
 ];
