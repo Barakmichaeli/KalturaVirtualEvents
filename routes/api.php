fa9d69de97469e7f7e9c8fd6b5c118843e9c2bd9
BarakMichaeli <barak.m@monolith.co.il>
2020-11-05 13:55:09 +0200

    npm run production

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header("Access-Control-Allow-Headers: *");
 
